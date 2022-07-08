cd4b0057698836f7d1118201bb5e754eafbb5d18
Jose Martinez Saldana <jmartinezsal326@gmail.com>
2022-07-07 21:35:27 -0700

    Login and Sign up forms finished

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SignUpForm from './components/auth/SignUpForm';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 import UserViewPage from './components/UserViewPage';
+import { loadPosts } from './store/post';
+import { loadComments } from './store/comment';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
   const dispatch = useDispatch();
+  const [loaded, setLoaded] = useState(false);
+  const user = useSelector(state => state.session.user);
 
   useEffect(() => {
     (async() => {
       await dispatch(authenticate());
+      await dispatch(loadPosts());
+      await dispatch(loadComments())
       setLoaded(true);
     })();
   }, [dispatch]);
@@ -26,6 +31,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        {user && }
         <Route path='/' exact={true}>
           <UserViewPage />
         </Route>
