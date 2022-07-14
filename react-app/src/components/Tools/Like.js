3c9bd78ed86ae4e75fc644f2801b765028c32afd
Jose Martinez Saldana <jmartinezsal326@gmail.com>
2022-07-13 23:27:43 -0700

    Changes made for efficiency

--- react-app/src/components/Tools/Like.js
+++ react-app/src/components/Tools/Like.js
@@ -1,27 +1,33 @@
+import { useState } from 'react';
 import { FaRegHeart } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 
 import { removeLike, createLike } from '../../store/like';
 import { loadPosts } from '../../store/post';
 
-function Like({ postId, liked, setLiked}) {
+function Like({ postId, liked, setLiked }) {
   const dispatch = useDispatch();
+  const [disable, setDisable] = useState();
 
   const onClickLiker = async () => {
-      if(liked){
-        await dispatch(removeLike(Object.keys(liked)[0]))
-        setLiked(false)
-      } else{
-        const like = await dispatch(createLike(postId))
-        const id = like.id
-        setLiked({[id]: true})
-      }
-      await dispatch(loadPosts());
+    setDisable(true)
+    if (liked) {
+      await dispatch(removeLike(Object.keys(liked)[0]))
+      setLiked(false)
+    } else {
+      const like = await dispatch(createLike(postId))
+      const id = like.id
+      setLiked({ [id]: true })
+    }
+    await dispatch(loadPosts());
+    setTimeout(function () {
+      setDisable(false)
+    }, [500])
   }
 
   return (
     <>
-      <FaRegHeart onClick={onClickLiker} style={liked ? { "backgroundColor": "red"} : {}} />
+      <FaRegHeart onClick={disable ? undefined : onClickLiker} style={liked ? { "color": "red" } : {}} />
     </>
   )
 }
